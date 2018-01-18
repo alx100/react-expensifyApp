cf75f2ffb58267cc602223c43b6fad088931bd3a
Alx100 <stotsky.alex@gmail.com>
2018-01-18 18:29:57 +0200

    Get firebase expenses on start

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense, removeExpense, editExpense } from './actions/expenses';
+import { startSetExpenses } from './actions/expenses';
 import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from './actions/filters';
 import expensesReducer from './reducers/expenses';
 import filtersReducer from './reducers/filters';
@@ -17,26 +17,18 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
-
 const store = configureStore();
-
-// store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
-// store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
-// store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
-
-
-// const state = store.getState();
-// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-// console.log(visibleExpenses);
-
-// console.log(store.getState());  
 
 const jsx = (
   <Provider store={store}>
     <AppRouter /> 
   </Provider>
 );
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(() => { 
+  ReactDOM.render(jsx, document.getElementById('app'));
+});
 
 
-ReactDOM.render(jsx, document.getElementById('app'));
 
