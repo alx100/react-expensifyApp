720f791a54cd460f349a4d90567124e74d34bbb6
Alx100 <stotsky.alex@gmail.com>
2018-01-18 16:45:20 +0200

    Setup test database env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyB2QeO07fL9t-nNVkDquW0S-cBpt7isKqw",
-  authDomain: "expensify-alx100.firebaseapp.com",
-  databaseURL: "https://expensify-alx100.firebaseio.com",
-  projectId: "expensify-alx100",
-  storageBucket: "expensify-alx100.appspot.com",
-  messagingSenderId: "108614517559"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
